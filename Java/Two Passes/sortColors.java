b69197f89c44f8a8c5a2652b5993f5b7616fdfe8
peterparit <paritv2003@gmail.com>
2025-05-17 21:14:13 +0700

    Added Java folder and a solution

---
+++ Java/Two Passes/sortColors.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int countReds = 0;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] == 0) {
+                countReds++;
+                left++;
+                continue;
+            } else {
+                while (nums[right] != 0 && left != right) right--;
+                if (nums[right] == 0 && left != right) {
+                    countReds++;
+                    nums[right] = nums[left];
+                    nums[left] = 0;
+                }
+            }
+            left++;
+        }
+        left = countReds;
+        right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] != 1) {
+                while (nums[right] != 1 && left != right) right--;
+                if (nums[right] == 1 && left != right) {
+                    nums[right] = nums[left];
+                    nums[left] = 1;
+                }
+            }
+            left++;
+        }
+    }
+}
